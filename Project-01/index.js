2e30f88260f6a71164ef24c626630f0ea85bc731
shubham-mali7 <shubhammali31299@gmail.com>
2024-03-27 12:14:29 -0700

    implemented restfull apis

--- Project-01/index.js
+++ Project-01/index.js
@@ -2,10 +2,14 @@
 
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 // instance of express
 const app = express();
 const PORT = 8000;
+
+// Middleware - Plugin
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.get("/users", (req, res) => {
@@ -41,7 +45,11 @@ app
 
 app.post("/api/users", (req, res) => {
   // TO DO: Create new user
-  return res.json({ status: "pending" });
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, response) => {
+    return res.json({ status: "success", id: users.length });
+  });
 });
 
 app.listen(PORT, () => {
