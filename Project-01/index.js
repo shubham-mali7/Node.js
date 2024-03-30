bb3d5578b63d0dfccf8e780a6fe471bb7de4feca
shubham-mali7 <shubhammali31299@gmail.com>
2024-03-30 14:05:21 -0700

    added status codes

--- Project-01/index.js
+++ Project-01/index.js
@@ -11,6 +11,13 @@ const PORT = 8000;
 // Middleware - Plugin
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  const log = `${Date.now()}: ${req.method}: ${req.path} Successfull\n`;
+  fs.appendFile("./log.txt", log, (err, data) => {
+    next();
+  });
+});
+
 // Routes
 app.get("/users", (req, res) => {
   const html = `
@@ -32,6 +39,9 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
     return res.json(user);
   })
   .patch((req, res) => {
@@ -46,6 +56,15 @@ app
 app.post("/api/users", (req, res) => {
   // TO DO: Create new user
   const body = req.body;
+  if (
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.profession
+  ) {
+    return res.status(400).json({ msg: "All fields are required!" });
+  }
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, response) => {
     return res.json({ status: "success", id: users.length });
